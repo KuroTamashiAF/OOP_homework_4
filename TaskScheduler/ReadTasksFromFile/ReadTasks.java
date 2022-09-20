5d3aeeee1ede6144648302daaead2a77a5044e5e
Alex <chugun777akk@mail.ru>
2022-09-20 10:34:00 +0700

    add class ReadTasks

--- TaskScheduler/ReadTasksFromFile/ReadTasks.java
+++ TaskScheduler/ReadTasksFromFile/ReadTasks.java
@@ -1,5 +1,15 @@
 package ReadTasksFromFile;
 
+import java.util.List;
+import java.util.ArrayList;
+import ObjectClasses.Task;
+
 public class ReadTasks {
     
+    public List<Task> listTasks(String path){
+
+
+        
+        return null;
+    }
 }
